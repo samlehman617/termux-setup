0c9d8439e9d73696b49622a3032b5a3ab88565f1
Sam Lehman <samlehman617@users.noreply.github.com>
2021-02-25 20:20:09 -0500

    Update setup-termux.sh

--- setup-termux.sh
+++ setup-termux.sh
@@ -1,9 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/sh
+
+# Show device info
 termux-info
 
 # Install packages
 pkg upgrade
 pkg install \
+	jq \
 	x11-repo \
 	proot
 
@@ -17,16 +20,26 @@ termux-setup-storage
 
 
 # Enable Termux on boot
-mkdir ~/.termux/boot
-echo "Before proceeding,you must do the following:"
-echo "1. Install the Termux:Boot app"
-echo "2. Go into Android's settings and turn off battery optimizations for Termux & Termux:Boot"
-echo "3. Launch the Termux:Boot application once."
+termux-dialog confirm \
+  -t "Enable Termux on boot"
+  -i \
+"Before setup can continue, you must:
+1. Install the Termux:Boot app.
+2. Open Android's settings.
+3. Turn off battery optimizations for Termux and Termux:Boot.
+4. Launch the Termux:Boot application at least once.
+
+Press 'Yes' when you have done so."
+
+#echo "Before proceeding,you must do the following:"
+#echo "1. Install the Termux:Boot app"
+#echo "2. Go into Android's settings and turn off battery optimizations for Termux & Termux:Boot"
+#echo "3. Launch the Termux:Boot application once."
 
 
 # Enable Termux services on boot
+mkdir -p ~/.termux/boot
 pkg install termux-services
-touch ~/.termux/boot/start-services
 cat > ~/.termux/boot/start-services << EOF
 #!/data/data/com.termux/files/usr/bin/sh
 termux-wake-lock
@@ -35,11 +48,12 @@ EOF
 
 
 # Configure shell
-chmod +x ~/scripts/setup-shell.sh
-~/scripts/setup-shell.sh
+chmod +x ./setup-shell.sh
+./setup-shell.sh
 
 # Handle root access
-root=$(termux-dialog confirm -t "Root Access" -i "Does your device have root access? If unsure, select 'no'")
+root=$(termux-dialog confirm -t "Root Access" -i "Does your device have root access? If unsure, select 'No'")
 
 
 echo "Done!"
+echo "Please restart Termux for changes to take effect."
